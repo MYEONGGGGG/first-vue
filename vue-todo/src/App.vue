802c51e2567557105f56bec7dc8094f8f8619a87
cme95 <66343251+cme95@users.noreply.github.com>
2022-02-16 13:42:27 +0900

    Initial commit

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+//컴포넌트 등록
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import TodoFooter from "./components/TodoFooter";
+
+
+export default {
+  components: {TodoFooter, TodoList, TodoInput, TodoHeader},
+  comments: {
+    //컴포넌트 태그명 : 컴포넌트 내용
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter
+  }
+}
+</script>
+
+<style>
+</style>
