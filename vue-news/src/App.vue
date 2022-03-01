62ccca28bdb1bd0dbc2fcdd7a679c97d89f074ca
choi <cme95@mentorit.co.kr>
2022-03-01 14:53:22 +0900

    ===== ESLint 플러그인 추가 및 설정 =====

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app"> Hello! </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    fetchDate() {
+      console.log('hello')
+    }
   }
 }
 </script>
